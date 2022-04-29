1eaa23bc4e9c2d5eac9d665f04072e666dd4a0f1
barcher <barcher@student.21-school.ru>
2022-04-29 16:00:56 +0300

    first comm

--- basic.go
+++ basic.go
@@ -1,89 +1,43 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"errors"
-	"fmt"
+	"log"
 	"net/http"
 	"os"
-	"strings"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-// Create a struct that mimics the webhook response body
-// https://core.telegram.org/bots/api#update
-type webhookReqBody struct {
-	Message struct {
-		Text string `json:"text"`
-		Chat struct {
-			ID int64 `json:"id"`
-		} `json:"chat"`
-	} `json:"message"`
-}
-
-// This handler is called everytime telegram sends us a webhook event
-func Handler(res http.ResponseWriter, req *http.Request) {
-	// First, decode the JSON response body
-	body := &webhookReqBody{}
-	if err := json.NewDecoder(req.Body).Decode(body); err != nil {
-		fmt.Println("could not decode request body", err)
-		return
-	}
-
-	// Check if the message contains the word "marco"
-	// if not, return without doing anything
-	if !strings.Contains(strings.ToLower(body.Message.Text), "marco") {
-		return
-	}
-
-	// If the text contains marco, call the `sayPolo` function, which
-	// is defined below
-	if err := sayPolo(body.Message.Chat.ID); err != nil {
-		fmt.Println("error in sending reply:", err)
-		return
-	}
-
-	// log a confirmation message if the message is sent successfully
-	fmt.Println("reply sent")
-}
-
-//The below code deals with the process of sending a response message
-// to the user
-
-// Create a struct to conform to the JSON body
-// of the send message request
-// https://core.telegram.org/bots/api#sendmessage
-type sendMessageReqBody struct {
-	ChatID int64  `json:"chat_id"`
-	Text   string `json:"text"`
-}
-
-// sayPolo takes a chatID and sends "polo" to them
-func sayPolo(chatID int64) error {
-	// Create the request body struct
-	reqBody := &sendMessageReqBody{
-		ChatID: chatID,
-		Text:   "Polo!!",
-	}
-	// Create the JSON body from the struct
-	reqBytes, err := json.Marshal(reqBody)
-	if err != nil {
-		return err
-	}
-
-	// Send a post request with your token
-	res, err := http.Post("https://api.telegram.org/bot1253239309:AAE0ViuDlerHUbALz7QRpXOsjLundw_eufo/sendMessage", "application/json", bytes.NewBuffer(reqBytes))
-	if err != nil {
-		return err
-	}
-	if res.StatusCode != http.StatusOK {
-		return errors.New("unexpected status" + res.Status)
-	}
-
-	return nil
-}
-
-// FInally, the main funtion starts our server on port 3000
 func main() {
-	http.ListenAndServe(":"+os.Getenv("PORT"), http.HandlerFunc(Handler))
+	bot, err := tgbotapi.NewBotAPI("MyAwesomeBotToken")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	wh, _ := tgbotapi.NewWebhook("https://www.example.com:" + os.Getenv("PORT") + "/" + bot.Token)
+
+	_, err = bot.Request(wh)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	info, err := bot.GetWebhookInfo()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if info.LastErrorDate != 0 {
+		log.Printf("Telegram callback failed: %s", info.LastErrorMessage)
+	}
+
+	updates := bot.ListenForWebhook("/" + bot.Token)
+	go http.ListenAndServeTLS("0.0.0.0:"+os.Getenv("PORT"), "cert.pem", "key.pem", nil)
+
+	for update := range updates {
+		log.Printf("%+v\n", update)
+	}
 }
